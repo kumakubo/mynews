2b4027f8b1424ea4af3263c905275a6be67cc6df
kumakubo <toqjd99408@yahoo.co.jp>
2023-10-03 05:21:18 +0000

    PHP/Laravel 11課題

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,14 +1,17 @@
-<!DOCUTYPE html>
-<html>
-    <head>
-        <meta charest="utf-8">
-        <meta http-equiv="X-UA-Compatible"
-        content="IE=edge">
-        <meta name="viewport" content="width=device-width,initial-scale=1">
-        
-        <title>プロフィール作成</title>
-    </head>
-    <body>
-        <h1>プロフィール作成画面</h1>
-    </body>
-</html>
+{{-- layouts/admin.blade.phpを読み込む --}}
+@extends('layouts.admin')
+
+
+{{-- admin.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
+@section('title', 'プロフィール作成画面')
+
+{{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>プロフィール作成画面</h2>
+            </div>
+        </div>
+    </div>
+@endsection
